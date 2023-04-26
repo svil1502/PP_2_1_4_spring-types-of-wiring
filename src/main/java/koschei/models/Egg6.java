0653ec4d9d273f506bb6e1a71aa247025899f961
Svetlana Ilina <developersvil@gmail.com>
2023-04-26 15:48:05 +0300

    first

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+    private final Needle7 needl;
+
+    @Autowired
+    public Egg6(Needle7 needl) {
+        this.needl = needl;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needl.toString();
     }
 }
